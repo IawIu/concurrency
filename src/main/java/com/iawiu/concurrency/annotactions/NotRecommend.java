dea43214240e631aac2bf04a5f0935578eb2ab45
tom_glb <tom_glb@126.com>
2018-06-20 14:57:06 +0800

    add annotactions

---
+++ src/main/java/com/iawiu/concurrency/annotactions/NotRecommend.java
@@ -0,0 +1,17 @@
+package com.iawiu.concurrency.annotactions;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记不推荐的类
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    String value() default "";
+}
---
+++ src/main/java/com/iawiu/concurrency/annotactions/NotThreadSafe.java
@@ -0,0 +1,17 @@
+package com.iawiu.concurrency.annotactions;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记线程不安全的类
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+}
